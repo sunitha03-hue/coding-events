5d8af8cacc98294ec19c20eb2156a81ea39906d3
sunitha <sunitha>
2020-10-04 22:53:27 -0500

    Started coding Chapter 11 completed upto 12.3.1

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,21 +17,27 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
-    @GetMapping
+   @GetMapping
     public String displayAllEvents(Model model){
         model.addAttribute("events", events);
         return "events/index";
     }
-    @GetMapping("create")
-    public String renderCreateEventForm(){
-        return "events/create";
-    }
-    @PostMapping("create")
+//    @GetMapping("create")
+//    public String renderCreateEventForm(){
+//        return "events/create";
+//    }
+      @GetMapping("create")
+      public String displayCreateEventForm(Model model){
+       model.addAttribute("title", "Create Event");
+      return "events/create";
+      }
 
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
+        events.add(new Event(eventName, eventDescription));
         return "redirect:";
        // return "redirect:/events";
     }
